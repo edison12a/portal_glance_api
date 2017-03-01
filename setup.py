a8807b660f01058f21932bafc166847e6021d28e
najar1412 <rory.jarrel@gmail.com>
2017-02-28 21:37:27 -0500

    .gitignore

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+# example
+"""
+setup(
+    name='flaskr',
+    packages=['flaskr'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
+"""
+
+from setuptools import setup
+
+setup(
+    name='glance',
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
