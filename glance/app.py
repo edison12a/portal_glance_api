6154a3e96cdac1d1996281a74e3bfc919d25d34e
najar1412 <rory.jarrel@gmail.com>
2017-05-02 21:13:01 -0400

    example code

--- glance/app.py
+++ glance/app.py
@@ -24,6 +24,42 @@ API_GEOMETRY = 'http://127.0.0.1:5050/glance/api/geometry'
 API_COLLECTION = 'http://127.0.0.1:5050/glance/api/collection'
 
 
+"""
+### EXAMPLE 'cart' - reverse code for 'collections'
+
+from flask import Blueprint, render_template, abort, session, flash, redirect, url_for
+
+@store_blueprint.route('/product/<int:id>', methods=['GET', 'POST'])
+def product(id=0):
+    # AddCart is a form from WTF forms. It has a prefix because there
+    # is more than one form on the page.
+    cart = AddCart(prefix="cart")
+
+    # This is the product being viewed on the page.
+    product = Product.query.get(id)
+
+
+    if cart.validate_on_submit():
+        # Checks to see if the user has already started a cart.
+        if 'cart' in session:
+            # If the product is not in the cart, then add it.
+            if not any(product.name in d for d in session['cart']):
+                session['cart'].append({product.name: cart.quantity.data})
+
+            # If the product is already in the cart, update the quantity
+            elif any(product.name in d for d in session['cart']):
+                for d in session['cart']:
+                    d.update((k, cart.quantity.data) for k, v in d.items() if k == product.name)
+
+        else:
+            # In this block, the user has not started a cart, so we start it for them and add the product.
+            session['cart'] = [{product.name: cart.quantity.data}]
+
+
+        return redirect(url_for('store.index'))
+
+"""
+
 @app.route('/')
 def home():
     if LoggedIn(session):
